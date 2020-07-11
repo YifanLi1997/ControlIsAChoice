ee3eaa610d20b1e8b1695ab40f584f2014c0d28d
李一帆 <liyifan@liyifandeMacBook-Pro.local>
2020-07-11 21:08:45 +0800

    Character Controller does not work with 2D, it is for 3D

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,64 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] Camera m_camera;
+
+    Animator m_animator;
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float rotateSpeed = 5f;
+    Vector3 moveDirection;
+
     void Start()
     {
-        
+        m_animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MoveForward();
+        Rotate();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision detected.");
+    }
+
+    private void Rotate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(transform.forward, rotateSpeed * Time.deltaTime);
+            m_animator.SetBool("IsSwimming", true);
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(transform.forward, -rotateSpeed * Time.deltaTime);
+            m_animator.SetBool("IsSwimming", true);
+        }
+    }
+
+    private void MoveForward()
+    {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            m_animator.SetBool("IsSwimming", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space)|| Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            m_animator.SetBool("IsSwimming", false);
+        }
+    }
+
+    void LateUpdate()
+    {
+        m_camera.transform.position = new Vector3(transform.position.x, transform.position.y, m_camera.transform.position.z);
     }
 }
